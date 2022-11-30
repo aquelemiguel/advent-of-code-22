0696fb0b92b7cbd0710c4b357e2aec890d12ee3c
aquelemiguel <aquelemiguel@gmail.com>
2022-11-30 14:32:10 +0000

    style: remove .rustfmt.toml

--- src/bin/prep.rs
+++ src/bin/prep.rs
@@ -1,47 +1,47 @@
 use std::fs;
 
 fn read_input(file_name: &str) -> Vec<i32> {
-  fs::read_to_string(file_name)
-    .unwrap()
-    .lines()
-    .map(|l| l.to_string().parse::<i32>().unwrap())
-    .collect()
+    fs::read_to_string(file_name)
+        .unwrap()
+        .lines()
+        .map(|l| l.to_string().parse::<i32>().unwrap())
+        .collect()
 }
 
 fn p1(values: &[i32]) -> i32 {
-  values.iter().sum()
+    values.iter().sum()
 }
 
 fn p2(values: &[i32]) -> i32 {
-  values.iter().fold(1, |acc, x| acc * x)
+    values.iter().fold(1, |acc, x| acc * x)
 }
 
 fn main() {
-  let values = read_input("input/prep-full");
+    let values = read_input("input/prep-full");
 
-  println!("p1: {}", p1(&values));
-  println!("p2: {}", p2(&values));
+    println!("p1: {}", p1(&values));
+    println!("p2: {}", p2(&values));
 }
 
 #[cfg(test)]
 mod tests {
-  use crate::*;
+    use crate::*;
 
-  #[test]
-  fn test_p1() {
-    let input_example = read_input("input/prep-example");
-    let input_full = read_input("input/prep-full");
+    #[test]
+    fn test_p1() {
+        let input_example = read_input("input/prep-example");
+        let input_full = read_input("input/prep-full");
 
-    assert_eq!(p1(&input_example), 7);
-    assert_eq!(p1(&input_full), 94);
-  }
+        assert_eq!(p1(&input_example), 7);
+        assert_eq!(p1(&input_full), 94);
+    }
 
-  #[test]
-  fn test_p2() {
-    let input_example = read_input("input/prep-example");
-    let input_full = read_input("input/prep-full");
+    #[test]
+    fn test_p2() {
+        let input_example = read_input("input/prep-example");
+        let input_full = read_input("input/prep-full");
 
-    assert_eq!(p2(&input_example), 8);
-    assert_eq!(p2(&input_full), 30000);
-  }
+        assert_eq!(p2(&input_example), 8);
+        assert_eq!(p2(&input_full), 30000);
+    }
 }
