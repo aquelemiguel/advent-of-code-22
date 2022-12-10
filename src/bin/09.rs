9bca7baeaf0854a9cf864e6548e7d314c39b0abf
aquelemiguel <aquelemiguel@gmail.com>
2022-12-10 05:14:29 +0000

    refactor: move input parsing to own func

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -9,15 +9,12 @@ fn follow(head: &Knot, tail: &Knot) -> Knot {
     if (head.0 - tail.0).abs() <= 1 && (head.1 - tail.1).abs() <= 1 {
         return knot;
     }
-
     if head.0 != tail.0 {
         knot.0 = tail.0 + (head.0 - tail.0).signum();
     }
-
     if head.1 != tail.1 {
         knot.1 = tail.1 + (head.1 - tail.1).signum();
     }
-
     knot
 }
 
@@ -32,16 +29,7 @@ fn move_head(head: &Knot, dir: char) -> Knot {
 }
 
 fn main() {
-    let cmds = fs::read_to_string("input/09.in")
-        .unwrap()
-        .lines()
-        .map(|m| {
-            m.split_whitespace()
-                .collect_tuple::<(&str, &str)>()
-                .map(|(p, s)| (p.chars().next().unwrap(), s.parse::<i32>().unwrap()))
-                .unwrap()
-        })
-        .collect_vec();
+    let cmds = read_input("input/09.in");
 
     let mut knots = vec![(0, 0); 10];
     let mut pos = vec![HashSet::<Knot>::new(); 10];
@@ -59,4 +47,17 @@ fn main() {
 
     println!("p1: {:?}", pos[1].len());
     println!("p2: {:?}", pos[9].len());
+}
+
+fn read_input(file_name: &str) -> Vec<(char, i32)> {
+    let cmds = fs::read_to_string(file_name).unwrap();
+
+    cmds.lines()
+        .map(|m| {
+            m.split_whitespace()
+                .collect_tuple::<(&str, &str)>()
+                .map(|(p, s)| (p.chars().next().unwrap(), s.parse::<i32>().unwrap()))
+                .unwrap()
+        })
+        .collect()
 }
